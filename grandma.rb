398a2561e3da6baeb610b14011a80da840d40d30
Adham Muhammad <admuaq@gmail.com>
2018-07-07 05:51:43 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -1,11 +1,11 @@
 # Write a speak_to_grandma method.
 def speak_to_grandma (response)
     
-  if (response == response.upcase)
-    return "NO, NOT SINCE 1938!"
-    
-  elsif (response == "I LOVE YOU GRANDMA!")
+  if (response == "I LOVE YOU GRANDMA!")
     return "I LOVE YOU TOO PUMPKIN!"
+  
+  elsif (response == response.upcase)
+    return "NO, NOT SINCE 1938!"
   
   else
     return "HUH?! SPEAK UP, SONNY!"
