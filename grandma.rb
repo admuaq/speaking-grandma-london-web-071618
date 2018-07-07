f2f956eab8889b9f010b130a54c7985be1ac032b
Adham Muhammad <admuaq@gmail.com>
2018-07-07 05:49:56 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -4,7 +4,7 @@ def speak_to_grandma (response)
   if (response == response.upcase)
     return "NO, NOT SINCE 1938!"
     
-  elsif (response == "I LOVE YOU GRANDMA!")
+  elsif (response === "I LOVE YOU GRANDMA!")
     return 'I LOVE YOU TOO PUMPKIN!'
   
   else
